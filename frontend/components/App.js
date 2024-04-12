0adfcefa43015e6c624de93a8dd17d624e80481a
KClower <kclower3@gmail.com>
2024-04-12 14:47:28 -0500

    added notes to understand the process better

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,68 +1,80 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
+  
+
+ // this is all JS  
 
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
-
+  // ❗ Create state to hold the data from the API using useState.
   const [data, setData] = useState([]);
+
   
 
-  // ❗ Create effects to fetch the data and put it in state
+    // ❗ Create effects to fetch the data and put it in state
 
+    // useEffect is how we fetch data from somewhere else outside the application.
+    // axios.get will hold the box(es) of data until it is unpacked.
+    // give the box(es) of data a new variable(s) to pass.
   useEffect(() => {
-   
     const people = axios.get('http://localhost:9009/api/people');
-    const planets =  axios.get('http://localhost:9009/api/planets');
+    const planets = axios.get('http://localhost:9009/api/planets');
 
+    // the .then function is how we unpack the box, using a response and a callback.
+    // use the new variable(s)
+    // if more than one axios call use Promise.all()
+    // if only one axios call just use newVariableName.then 
     Promise.all([people, planets]).then((response) => {
-      const [peopleResponse, planetsResponse] = response;
-      
-      // map thru the people array, 
-      // inside the map use .find to search thru the planets array,
-      // create a new object containing data from person object and planet object,
-      // take result of map and assign to state.
-
-     const charaters = peopleResponse.data.map((person) => {
-      const planet = planetsResponse.data.find(planet => person.homeworld === planet.id) 
-      return {
-        id: person.id,
-        name: person.name,
-        homeworld: {
-          id: planet.id,
-          name: planet.name,
-        }
+      // the response is how we search through the data to locate the specific data we require, in the console.
+      // use dot notation to reach the next level of data if needed.
+      // console.log the response to achive this.
+     console.log("these are the people", response)
+     console.log("these are the planets", response)
+      // give the response(s) a new variable(s) to pass.
+     const [peopleResponse, planetResponse] = response;
+   
+      // map thru the new people array.
+      // provide the new array a name.
+     const characters = peopleResponse.data.map((person) => {
+      // inside the map use .find to search thru the new planets array to match the homeworld and planet. 
+     const planet = planetResponse.data.find(planet => person.homeworld === planet.id)
+      // create a new object containing data that we need from person object and planet object,
+     return{
+      id: person.id,
+      name: person.name,
+      homeworld: {
+        id: planet.id,
+        name: planet.name,      
       }
-     })
-     setData(charaters)
+    }
+    })
+     // take result of map and assign to state.
+     setData(characters)
     })
   }, []);
 
- if(data.lenght === 0){
-  return <div>Loading..</div>
- }
+         //     NOW IS TIME TO BUILD THE CHARACTER !!!
 
-
-  return (
-    
-    <div>
-      <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {data.map((charater) => {
-        return (
-          <Character
-          name = {charater.name}
-          homeWorld = {charater.homeworld.name}
-          />
-        );
-       })}
-    </div>
-  
-   
-  )
+      // after the return we are in REACT.
+ 
+      return (
+       <div>
+        <h2>Star Wars Characters</h2>
+        <p>See the README of the project for instructions on completing this challenge</p>
+        {data.map((character) => {
+          return (
+            <Character
+            name = {character.name}
+            homeWorld = {character.homeworld.name}
+            />
+          )
+        })}
+        
+       </div>
+      )
 }
 
 
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -2,40 +2,41 @@ import React, {useState} from 'react'
 
 
 function Character(props){ // ❗ Add the props
-const {homeWorld, name} = props
+   // what are the pieces of data we want to be our props?
+ const {name, homeWorld} = props
 
-const [showHomeWorld, setShowHomeWorld] = useState(false);
+  // ❗ Create a state to hold whether the homeworld is rendering or not
+ const [showHomeWorld, setShowHomeWorld] = useState(false)
 
-function handleClick(){
+   // ❗ Create a "toggle" click handler to show or remove the homeworld
+ function handleClick() {
+  // since the default of state is set false this will make the state true apon click
   setShowHomeWorld(
     !showHomeWorld
   )
-}
+ }
 
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
-  return (
-    
-    <div className = "character-card" onClick={handleClick}>
-      <h3 className = "character-name">{name}</h3>
-      {showHomeWorld && <HomeWorld homeWorldName={homeWorld}/>}
-      
-      {showHomeWorld && (
-          <p>Planet: 
-          <span className = "character-planet">{homeWorld}</span>
-          </p>
-      )}
-    </div>
-  )
+ return(
+ // this will put the click function into effect when clicked anywhere on the card
+ // name will be passed in from the parent 
+ // showHomeWorld will appear from state once clicked the function HomeWorld will hold what is being rendered
+ <div className="character-card" onClick={handleClick}>
+ <h3 className="character-name">{name}</h3>
+ {showHomeWorld && <HomeWorld homeWorldName={homeWorld}/>}
+ </div>
+ )
 }
 
 function HomeWorld(props){
- 
-  return (
-    <p>Planet: 
-    <span className = "character-planet">{props.homeWorldName}</span>
-    </p>
+  return(
+  <p>Planet:
+    <span className="character-planet">{props.homeWorldName}</span>
+  </p>
   )
-}
+ }
+  
+ 
 
-export default Character
+
+
+export default Character;
